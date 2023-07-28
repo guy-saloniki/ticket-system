3981783db1ab163677edd4b01c9ee4794a3e6137
guy-saloniki <guysalonik@walla.com>
2023-07-28 18:56:55 +0300

    Added backend authentication

---
+++ backend/BLL/usersBLL.js
@@ -0,0 +1,88 @@
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// Register a new user
+// Route - POST /api/users/register
+// Public
+const register = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill all fields');
+  }
+
+  // Find if user already exists
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (!user) {
+    res.status(400);
+    throw new Error('Something went wrong...');
+  }
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    token: generateToken(user._id),
+  });
+});
+
+// Login user
+// Route - POST /api/users/login
+// Public
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check user and password validation
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    req.status(401);
+    throw new Error('User not found');
+  }
+});
+
+// Get user profile
+// Route - GET /api/users/me
+// Private
+const getMe = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json({ id: req.user.id, name: req.user.name, email: req.user.email });
+});
+
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+module.exports = {
+  register,
+  login,
+  getMe,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const usersRoute = require('./routes/userRoutes');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 8000;
+
+// Connect to db
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/api/users', usersRoute);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port: ${PORT}`);
+});
